1d2955b4664ad854fc8f2c2a956583c5a50b12a3
Matthew Nguyen <m.nguyen032204@gmail.com>
2026-01-29 10:21:19 -0600

    New VectorPos class for flocking algorithm

--- visualstudio/Squad.h
+++ visualstudio/Squad.h
@@ -8,6 +8,40 @@ enum State {
 	ATTACK, DEFEND, RETREAT, IDLE, POSITIONING
 };
 
+class VectorPos : public BWAPI::Point<double, 1> {
+public:
+	VectorPos(double _x, double _y) : BWAPI::Point<double, 1>(_x, _y) {}
+
+	VectorPos& operator=(const VectorPos& other) noexcept(true) {
+		this->x = other.x;
+		this->y = other.y;
+		return *this;
+	}
+	VectorPos& operator=(const BWAPI::Point<int, 1> other) noexcept(true) {
+		this->x = (double) other.x;
+		this->y = (double) other.y;
+		return *this;
+	}
+	VectorPos& operator=(const BWAPI::Point<double, 1> other) noexcept(true) {
+		this->x = (double)other.x;
+		this->y = (double)other.y;
+		return *this;
+	}
+
+	VectorPos operator-(const VectorPos& rhs) const {
+		return VectorPos(this->x - rhs.x, this->y - rhs.y);
+	}
+	VectorPos operator+(const VectorPos& rhs) const {
+		return VectorPos(this->x + rhs.x, this->y + rhs.y);
+	}
+	VectorPos operator*(const double& scalar) const {
+		return VectorPos(this->x * scalar, this->y * scalar);
+	}
+	VectorPos operator*(const int& scalar) const {
+		return VectorPos(this->x * scalar, this->y * scalar);
+	}
+};
+
 class Squad {
 public:
 	int squadId;
@@ -16,9 +50,10 @@ public:
 	BWAPI::Unit leader;
 	std::vector<BWAPI::Unit> units;
 	State state;
-	double minNDistance = 40;
 	Path currentPath;
 	int currentPathIdx = 0;
+	double minNDistance = 80;
+	double minSepDistance = 30;
 
 	Squad(BWAPI::Unit leader, int squadId, BWAPI::Color squadColor, int unitSize);
 
@@ -43,6 +78,6 @@ public:
 	// Utility functions
 	void drawCurrentPath();
 	double getMagnitude(BWAPI::Position vector);
-	BWAPI::Position normalize(BWAPI::Position vector);
+	VectorPos normalize(VectorPos vector);
 	void drawDebugInfo();
 };
