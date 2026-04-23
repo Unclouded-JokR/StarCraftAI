3466905ebf247b5e3f748303b9b2c1dd8441632e
Matthew Nguyen <m.nguyen032204@gmail.com>
2026-04-23 18:25:05 -0500

    Hash documented

--- src/protobot/Hashes.h
+++ src/protobot/Hashes.h
@@ -2,35 +2,45 @@
 #include <BWAPI.h>
 #include <bwem.h>
 
-// Hash for storing units as keys
+/// <summary>
+/// Hash for storing units as keys
+/// </summary>
 struct unitHash {
 	std::size_t operator()(const BWAPI::Unit& unit) const {
 		return std::hash<int>{}(unit->getID());
 	}
 };
 
-// Hash for storing BWAPI::TilePositions 
+/// <summary>
+/// Hash for storing BWAPI::TilePositions 
+/// </summary>
 struct PositionHash {
 	std::size_t operator()(const BWAPI::Position& tile) const {
 		return std::hash<int>()(0.5 * (tile.x + tile.y) * (tile.x + tile.y + 1) + tile.y);
 	}
 };
 
-// Hash for storing BWAPI::TilePositions 
+/// <summary>
+/// Hash for storing BWAPI::TilePositions 
+/// </summary>
 struct TilePositionHash {
 	std::size_t operator()(const BWAPI::TilePosition& tile) const {
 		return std::hash<int>()(0.5 * (tile.x + tile.y) * (tile.x + tile.y + 1) + tile.y);
 	}
 };
 
-// Hash for storing AreaId pairs as keys
+/// <summary>
+/// Hash for storing AreaId pairs as keys
+/// </summary>
 struct AreaIdPairHash {
 	std::size_t operator()(const std::pair<BWEM::Area::id, BWEM::Area::id>& v) const {
 		return std::hash<int>{}(v.first) ^ std::hash<int>{}(v.second);
 	}
 };
 
-// Hash used for storing <AreaId, Area> pairs
+/// <summary>
+/// Hash used for storing <AreaId, Area> pairs
+/// </summary>
 struct AreaIdHash {
 	std::size_t operator()(const BWEM::Area::id v) const {
 		return std::hash<int>{}(v);
